4e1941804337ee8777c2c0f12c927d757b77380c
irineufreitas <freitaschoro@gmail.com>
2024-05-15 05:32:30 +0200

    error

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const errorHandlingMiddleware = require('./errorHandlingMiddleware');
 
-// Define your routes
+
 const indexRoute = require('./routes/index');
 const errorRoute = require('./routes/error');
 
-// Mount the error handling middleware
+
 app.use(errorHandlingMiddleware);
 
-// Mount your routes
 app.use('/', indexRoute);
-app.use('/error', errorRoute); // Assuming your error route is mounted under /error path
+app.use('/error', errorRoute); 
 
 module.exports = app;
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,26 @@
+// errorController.js
+
+const path = require('path');
+
 const errorController = {};
 
+// Controller function to trigger an intentional error
 errorController.triggerError = (req, res, next) => {
-    console.log(undefinedVariable); 
+    try {
+        // Simulate an error by accessing an undefined variable
+        console.log(undefinedVariable); // This will throw an error
+    } catch (error) {
+        // Pass the error to the error handling middleware
+        next(error);
+    }
+};
+
+// Controller function to handle 500 errors
+errorController.handle500Error = (err, req, res, next) => {
+    res.status(500).render(path.join(__dirname, '../views/error/error-trigger.ejs'), {
+        title: 'Error',
+        year: new Date().getFullYear() 
+    });
 };
 
 module.exports = errorController;
