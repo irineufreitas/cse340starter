aef739ddb5dde7697fb9c4a7084c23d28e4e3688
irineufreitas <freitaschoro@gmail.com>
2024-05-16 22:35:49 +0200

    error fix

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,4 @@
-// errorController.js
+
 
 const path = require('path');
 
@@ -7,15 +7,14 @@ const errorController = {};
 // Controller function to trigger an intentional error
 errorController.triggerError = (req, res, next) => {
     try {
-        // Simulate an error by accessing an undefined variable
+       
         console.log(undefinedVariable); // This will throw an error
     } catch (error) {
-        // Pass the error to the error handling middleware
+       
         next(error);
     }
 };
 
-// Controller function to handle 500 errors
 errorController.handle500Error = (err, req, res, next) => {
     res.status(500).render(path.join(__dirname, '../views/error/error-trigger.ejs'), {
         title: 'Error',
